63d089be6490626af692c8a4fed8835c7aadf88e
chlablak <chlablak@gmail.com>
2016-03-23 12:54:37 +0100

    Basic client-server architecture

---
+++ src/CaesarProtocol/src/main/java/ch/heigvd/res/caesar/server/ServantWorker.java
@@ -0,0 +1,67 @@
+/**
+ * @file ServantWorker.java
+ * @brief Manage the conversation with a single client
+ * @author Patrick Champion, Damien Rochat
+ */
+
+package ch.heigvd.res.caesar.server;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServantWorker implements Runnable {
+
+    private static final Logger LOG = Logger.getLogger(ServantWorker.class.getName());
+
+    // The client socket
+    private Socket client;
+
+    /**
+     * Constructor
+     * @param client - the client socket
+     */
+    public ServantWorker(Socket client) {
+        this.client = client;
+    }
+
+    /**
+     * Conversation with the client
+     */
+    @Override
+    public void run() {
+        try {
+            // print client info
+            LOG.info("Client info: " + client);
+
+            // get streams from/to client
+            InputStream fromClient = client.getInputStream();
+            OutputStream toClient = client.getOutputStream();
+
+            while(true) {
+                // wait for a message
+                //String message = fromClient.read();
+
+                // quit on empty message
+                //if(message.isEmpty())
+                //    break;
+
+                // echo the message
+                //toClient.write(message);
+
+                break; // a enlever
+            }
+
+            // end, close socket
+            LOG.info("Client quit: " + client);
+            client.close();
+
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+}
